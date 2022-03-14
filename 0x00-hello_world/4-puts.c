d21b8b56b9bd3e368001f2c26aa0093f93b4babf
Edemkj <smith27.ns@gmail.com>
2022-03-13 20:04:58 -0700

    Program prints to the screen without using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
-include <stdio.h>
+#input <stdio.h>
 int main(void)
-{	puts("\"Programming is like building a multiple puzzle);
+{
+	puts("\"Programming is like building a multiple puzzle");
 	return(0);
 }
